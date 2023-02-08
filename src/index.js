0ca6927c31ed12e3ccf436e22186bf55084a42d6
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-08 09:42:46 -0700

    finished the project, was able to send a patch and save state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+let currGame;
+
 fetch('http://localhost:3000/games')
 .then(resp => resp.json())
 .then(data =>{
     populateGameList(data)
-    renderFirstGame(data[0])
+    renderGame(data[0])
     console.log(data)
 })
 
 const titlePlace = document.querySelector('h2#detail-title');
 const scorePlace = document.querySelector('span#detail-high-score');
 const imgPlace = document.querySelector('#detail-image');
+const highScoreForm = document.querySelector('#high-score-form')
+
 
 function populateGameList(names){
     const gameListNav = document.querySelector('nav.game-list')
@@ -19,16 +23,41 @@ function populateGameList(names){
         gameListNav.appendChild(h5);
 
         h5.addEventListener('click', () => {
-            renderFirstGame(item)
+            renderGame(item)
         })
     })
 
 }
 
-function renderFirstGame(game){
-    titlePlace.innerText = game.name;
-    scorePlace.innerText = game.high_score;
-    imgPlace.src = game.image;
+function renderGame(game){
+    currGame = game;
+    titlePlace.innerText = currGame.name;
+    scorePlace.innerText = currGame.high_score;
+    imgPlace.src = currGame.image;
+    
 }
+
+
+highScoreForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    currGame.high_score = parseInt(e.target['score-input'].value)
+    renderGame(currGame)
+    // scorePlace.innerText = parseInt(e.target['score-input'].value)+parseInt(scorePlace.innerText);
+    fetch(`http://localhost:3000/games/${currGame.id}`, { 
+        method: 'PATCH', 
+        header:{
+            'content-type' : 'application/json'
+        },
+        body : JSON.stringify({
+            high_score : currGame.high_score
+        })
+    })
+    .then((resp)=>resp.json())
+    .then((data)=> {
+        console.log(data)
+        // renderGame(data)
+    })//scorePlace.innerText = data.high_score)
+})
+
 
 
